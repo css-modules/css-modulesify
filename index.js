42665df9ee575150f3127649eaf697916e8f9a67
tjallingt <tjallingt@gmail.com>
2016-02-09 13:13:54 +0100

    Corrected ordering on rootDir options

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ module.exports = function (browserify, options) {
   options = options || {};
 
   // if no root directory is specified, assume the cwd
-  var rootDir = options.rootDir || browserify._options.basedir || options.d;
+  var rootDir = options.rootDir || options.d || browserify._options.basedir;
   if (rootDir) { rootDir = path.resolve(rootDir); }
   if (!rootDir) { rootDir = process.cwd(); }
 
