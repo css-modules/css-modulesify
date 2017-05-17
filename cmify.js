cda3b4b8959fc063962fe9b9b52d8b2b4bdb7e74
Michael Ledin <m.ledin@appheads.ru>
2017-05-17 15:34:03 +0300

    eslint fixes

--- cmify.js
+++ cmify.js
@@ -3,7 +3,7 @@ var path = require("path");
 var util = require("util");
 
 util.inherits(Cmify, stream.Transform);
-function Cmify(filename, opts) {
+function Cmify (filename, opts) {
   if (!(this instanceof Cmify)) {
     return new Cmify(filename, opts);
   }
@@ -11,24 +11,24 @@ function Cmify(filename, opts) {
   stream.Transform.call(this);
 
   this.cssFilePattern = new RegExp(opts.cssFilePattern || '\.css$');
-  this._data = "";
+  this._data = '';
   this._filename = filename;
   this._cssOutFilename = opts.cssOutFilename;
 }
 
 Cmify.prototype.isCssFile = function (filename) {
-  return this.cssFilePattern.test(filename)
-}
+  return this.cssFilePattern.test(filename);
+};
 
 Cmify.prototype._transform = function (buf, enc, callback) {
   // only handle .css files
   if (!this.isCssFile(this._filename)) {
-    this.push(buf)
-    return callback()
+    this.push(buf);
+    return callback();
   }
 
-  this._data += buf
-  callback()
+  this._data += buf;
+  callback();
 };
 
-module.exports = Cmify
+module.exports = Cmify;
